f9a8ab888255d84ff8c0c09ffed5a0c4ce60c592
Matteo Tomasini <matteo.tomasini@gu.se>
2022-08-18 10:01:31 +0200

    Defined class card.

--- packOfCards.py
+++ packOfCards.py
@@ -4,4 +4,9 @@
 #   The goal of this script is to get some exercise with unit testing. 
 # 
 #   Author: Matteo Tomasini
-#   Date of start: 
+#   Date of start: 18.08.2022
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
