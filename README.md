003a18d667b393a4bfa935b2f58d458a35a96e69
Matteo Tomasini <matteo.tomasini@gu.se>
2022-08-18 09:57:33 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ...and a pack of cards!
+
+In this project, I learn to code a pack of card and implement different games. The goal of this work is to learn and train unit testing.
---
+++ packOfCards.py
@@ -0,0 +1,7 @@
+#   LEARNING TO TEST
+#   ----------------
+#   I want to create a pack of cards in this script, then maybe define some luck games later on!
+#   The goal of this script is to get some exercise with unit testing. 
+# 
+#   Author: Matteo Tomasini
+#   Date of start: 
